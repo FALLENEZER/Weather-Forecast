e1024f43f053962134e66180c08252227c41fe3c
FALLENEZER <igumnov.danechka@mail.ru>
2024-02-03 06:37:57 +0300

    Initial Commit

---
+++ Chat_Prototype/Chat_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chat_PrototypeApp.swift
+//  Chat_Prototype
+//
+//  Created by Даниил Игумнов on 03.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Chat_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat_Prototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chat_Prototype
+//
+//  Created by Даниил Игумнов on 03.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
