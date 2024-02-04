fc01ab936b7be94bbc27ad0eb6d2dee3f4a6a0fb
FALLENEZER <igumnov.danechka@mail.ru>
2024-02-04 11:09:36 +0300

    Working version

--- Chat_Prototype/ContentView.swift
+++ Chat_Prototype/ContentView.swift
@@ -9,16 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            VStack {
+                DayForecast(day: "Mon", isRainy: false, high: 70, low: 50 )
+                
+                DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
+                
+                DayForecast(day: "Wed", isRainy: true, high: 30, low: 10)
+                
+                DayForecast(day: "Thr", isRainy: false, high: 45, low: 35)
+                
+                DayForecast(day: "Fri", isRainy: true, high: 50, low: 10)
+                
+                DayForecast(day: "Sat", isRainy: false, high: 45, low: 5)
+                DayForecast(day: "Sun", isRainy: true, high: 70, low: 60)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    var iconName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.yellow
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(Font.headline)
+                
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(Font.largeTitle)
+                .padding(5)
+            Text("High: \(high)º")
+                .fontWeight(Font.Weight.semibold)
+            Text("Low: \(low)º")
+                .fontWeight(Font.Weight.medium)
+                .foregroundStyle(Color.secondary)
+        }
+        .padding()
+    }
 }
